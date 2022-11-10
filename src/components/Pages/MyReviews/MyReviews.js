7ebaf0a2a39928f1d37937ebb81c20c7c8cda2bd
unknown <marufmozumder7@gmail.com>
2022-11-10 13:45:03 +0100

    my-reviews-committed

--- src/components/Pages/MyReviews/MyReviews.js
+++ src/components/Pages/MyReviews/MyReviews.js
@@ -1,6 +1,7 @@
 import React, { useContext, useState } from 'react';
 import { Helmet } from 'react-helmet-async';
-import { useLoaderData } from 'react-router-dom';
+import { Link, useLoaderData } from 'react-router-dom';
+import Swal from 'sweetalert2';
 import { AuthContext } from '../../../UserContext/UserContext';
 import IndividualReview from './IndividualReview';
 
@@ -8,22 +9,52 @@ const MyReviews = () => {
     const myReviews = useLoaderData();
     const [reviews, setReviews] = useState(myReviews)
 
+    //Sweet Alert
+    const swalWithBootstrapButtons = Swal.mixin({
+        customClass: {
+          confirmButton: 'btn btn-success',
+          cancelButton: 'btn btn-danger'
+        },
+        buttonsStyling: false
+      })
+
     const handleDelete = (id) => {
-        const confirmation = window.confirm('Are you sure you want to delete this review?')
-        if(confirmation){
-            fetch(`http://localhost:5000/reviews/${id}`,{
+        swalWithBootstrapButtons.fire({
+            title: 'Are you sure?',
+            text: "You won't be able to undo this!",
+            icon: 'warning',
+            showCancelButton: true,
+            confirmButtonText: 'Yes, delete it!',
+            cancelButtonText: 'No, cancel!',
+            reverseButtons: true
+          }).then((result) => {
+            if (result.isConfirmed) {
+              swalWithBootstrapButtons.fire(
+                'Deleted!',
+                'Your file has been successfully deleted.',
+                'success'
+              )
+              fetch(`http://localhost:5000/reviews/${id}`,{
                 method: 'DELETE'
             })
             .then(res => res.json())
             .then(data => {
                 if(data.deletedCount > 0){
-                    alert('user deleted successfully')
                     const remaining = reviews.filter(singleReview => singleReview._id !== id);
                         setReviews(remaining);
                 }
             })
-        }
-        
+            } else if (
+              /* Read more about handling dismissals below */
+              result.dismiss === Swal.DismissReason.cancel
+            ) {
+              swalWithBootstrapButtons.fire(
+                'Cancelled',
+                "Your file hasn't been deleted :)",
+                'error'
+              )
+            }
+          })       
     }
     console.log(reviews);
 
@@ -37,12 +68,20 @@ const MyReviews = () => {
             <Helmet>
                 <title>My Reviews</title>
             </Helmet>
-            <div className='grid lg:grid-cols-3 md:grid-cols-2 sm:grid-cols-1'>
             {
-                reviews.map(myReview => <IndividualReview key={myReview._id} myReview={myReview} handleDelete={handleDelete}></IndividualReview>)
+                reviews.length > 0?
+                <div className='grid lg:grid-cols-3 md:grid-cols-2 sm:grid-cols-1'>
+                {
+                    reviews.map(myReview => <IndividualReview key={myReview._id} myReview={myReview} handleDelete={handleDelete}></IndividualReview>)
+                }
+                </div>
+                :
+                 <div className='mx-auto my-72'>
+                     <h3 className='text-cyan-800 text-6xl mb-24'>Oops! No Reviews found!</h3>
+                     <h4 className='text-4xl'>Click <Link className='text-cyan-800' to='/services'>here</Link> to add a review</h4>
+                </div>
             }
-            </div>
-            
+           
         </div>
     );
 };
